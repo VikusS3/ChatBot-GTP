15478b73ebf32681c2c83f57f419b0e8e5d4dae5
Saul V3 <felix21soc@gmail.com>
2023-04-27 17:09:41 -0500

    og no

--- server/server.js
+++ server/server.js
@@ -28,14 +28,13 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      //model: "gpt-3.5-turbo",
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0, // Los valores más altos significan que el modelo asumirá más riesgos.
-      max_tokens: 3000, // El número máximo de tokens a generar en la finalización. La mayoría de los modelos tienen una longitud de contexto de 2048 tokens (excepto los modelos más nuevos, que admiten 4096).
-      top_p: 1, // alternativa al muestreo con temperatura, llamado muestreo de núcleo
-      frequency_penalty: 0.5, // Número entre -2.0 y 2.0. Los valores positivos penalizan los tokens nuevos en función de su frecuencia existente en el texto hasta el momento, lo que reduce la probabilidad de que el modelo repita la misma línea palabra por palabra.
-      presence_penalty: 0, //Número entre -2.0 y 2.0. Los valores positivos penalizan los nuevos tokens en función de si aparecen en el texto hasta el momento, lo que aumenta la probabilidad de que el modelo hable sobre nuevos temas. .
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
     res.status(200).send({
