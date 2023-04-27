4af3eec60402c6df74be2cc41736cb6a63514991
Saul V3 <felix21soc@gmail.com>
2023-04-27 17:11:57 -0500

    la cage

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ import { Configuration, OpenAIApi } from 'openai'
 
 dotenv.config()
 
- 
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -19,7 +17,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hola desde bot-programer'
+    message: 'Hello from CodeX!'
   })
 })
 
